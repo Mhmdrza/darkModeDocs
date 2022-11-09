e0da4a639bd30b3312c9e74a264f6eb984523d7a
MohammadReza <mohammadreza.ala@snapp.cab>
2022-11-09 17:59:42 +0330

    initial commit

---
+++ ext.js
@@ -0,0 +1,20 @@
+
+// setting slider value frst time
+chrome.storage.sync.get([KEY], function(result) {
+    slider.value = result[KEY] || 0.95
+});
+function setKey (keyNama, keyVale) {
+    chrome.storage.sync.set({ [keyNama]: keyVale }, function() {});
+}
+slider.onchange = function sliderHnadler (e) {
+    setKey(KEY, e.target.value)
+}
+// change listener
+chrome.storage.onChanged.addListener(function (changes, namespace) {
+    for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
+        if ( key === KEY ) {
+            newValue && (slider.value = newValue);
+            newValue && setKey(KEY, newValue)
+        }
+    }
+});
---
+++ logic.js
@@ -0,0 +1,16 @@
+const KEY = 'darkness';
+function setCssVariable ( varName, varValue ) {
+    document.documentElement.style.setProperty(`--${varName}`, varValue);
+}
+// setting dark mode first time
+chrome.storage.sync.get([KEY], function(result) {
+    setCssVariable(KEY, result[KEY] || '0.95')
+});
+// set new value on change -- triggerd from html
+chrome.storage.onChanged.addListener(function (changes, namespace) {
+    for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
+        if ( key === KEY ) {
+            newValue && setCssVariable(KEY, newValue)
+        }
+    }
+});
